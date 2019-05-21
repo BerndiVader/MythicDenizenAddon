a5b3ab82483328684bcc63f7e7ffe88aabd12c37
BerndiVader <kasermandel@gmail.com>
2019-05-21 17:08:53 +0200

    added DenizenCustomObjective for Quests

--- src/main/java/com/gmail/berndivader/mythicdenizenaddon/MythicDenizenPlugin.java
+++ src/main/java/com/gmail/berndivader/mythicdenizenaddon/MythicDenizenPlugin.java
@@ -1,15 +1,27 @@
 package com.gmail.berndivader.mythicdenizenaddon;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MythicDenizenPlugin extends JavaPlugin {
-	private static MythicDenizenPlugin plugin;
-    private static SupportManager supportManager;
-    public enum Dependings {
+public
+class 
+MythicDenizenPlugin
+extends
+JavaPlugin 
+{
+	static MythicDenizenPlugin plugin;
+    static SupportManager supportManager;
+    enum Dependings {
     	MythicMobs,
-    	Denizen
+    	Denizen,
+    	Quests
     }
     
 	public static MythicDenizenPlugin inst() {
@@ -26,6 +38,8 @@ public class MythicDenizenPlugin extends JavaPlugin {
 		} else if (pm.getPlugin(Dependings.MythicMobs.toString()) == null) {
 			initFail("MythicMobs not found!");
 			return;
+		} else if(pm.getPlugin(Dependings.Quests.name())!=null) {
+			writeDenizenCustomObjective();
 		}
     	String strMMVer = Bukkit.getServer().getPluginManager().getPlugin(Dependings.MythicMobs.toString()).getDescription().getVersion();
 		int mmVer = Integer.valueOf(strMMVer.replaceAll("[\\D]", ""));
@@ -54,6 +68,25 @@ public class MythicDenizenPlugin extends JavaPlugin {
 		getLogger().warning("There was a problem registering MythicMobs for Denizen!");
 		e.printStackTrace();
 		getPluginLoader().disablePlugin(this);
+	}
+	
+	static void writeDenizenCustomObjective() {
+		File target=new File(plugin.getDataFolder().toString().replace("\\"+plugin.getName(),"")+"/Quests/modules/DenizenCustomQuestsObjective.jar");
+		if(!target.exists()) {
+			URL url=plugin.getClassLoader().getResource("DenizenCustomQuestsObjective.jar");
+			try {
+				InputStream in_stream=url.openStream();
+				try (FileOutputStream out_stream=new FileOutputStream(target)){
+					byte[]buffer=new byte[1024];
+					int i1;
+					while((i1=in_stream.read(buffer))!=-1){
+						out_stream.write(buffer,0,i1);
+					}
+				};
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
